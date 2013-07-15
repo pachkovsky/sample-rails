8db0bfda6da4067d9f5b0c1fd496265739804604
Pavel Pachkovskij <pavel.pachkovskij@gmail.com>
2013-07-15 15:43:13 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+  first_name: 'John',
+  last_name: 'Smith',
+  email: 'john.smith@gmail.com'
+)
